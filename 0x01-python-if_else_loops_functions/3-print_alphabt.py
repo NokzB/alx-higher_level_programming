3d48e1d8a1bf26dba02de5a3797d78c87f385daf
Noku <nnblb58@gmail.com>
2022-12-06 22:48:52 -0800

    corrected code to print letters qithout qe

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+for c in range(ord('a'), ord('z')+1):
+    if c is not (ord('q')) and c is not (ord('e')):
+        print("{}".format(chr(c)), end="")
