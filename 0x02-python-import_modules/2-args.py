eb12830f35013c7c04f60a69f6240e8b7993ac52
Nokuphila Bhembe <nnblb58@gmail.com>
2023-03-09 18:56:17 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     import sys
     
     counter = len(sys.argv) - 1
-    if count == 0:
+    if counter == 0:
         print("0 arguments.")
-    elif count == 1:
+    elif counter == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
+        print("{} arguments:".format(counter))
+    for i in range(counter):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
