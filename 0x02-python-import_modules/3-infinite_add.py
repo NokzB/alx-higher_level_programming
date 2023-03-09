43985f23289a8932dd86234a02f45335b383f3cc
Nokuphila Bhembe <nnblb58@gmail.com>
2023-03-09 19:01:11 +0200

    initial commit for tasks 3 - 5

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    from calculator_1 import add
+    a = input(int(a))
+    b = input(int(b))
+
+    print("{}".format(add(a, b)))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    from variables_load_5.py import a
+    print({}.format(a))
