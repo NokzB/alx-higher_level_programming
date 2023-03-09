ec5d4e4b9ccc9a27fe5f1ff3f10193cd243aec04
Noku <nnblb58@gmail.com>
2023-03-09 08:49:14 -0800

    made file executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,16 @@
 from sys import argv
 
 def main():
-    print("{} argument".format(len(argv) - 1), end='')
+
+    print("{} argument".format(len(argv) - 1))
+
     if len(argv) == 1:
         print('s.')
     elif len(argv) == 2:
         print(':')
     else:
         print('s:')
+
     for i in range(1, len(argv)):
         print('{}: {}'.format(i, argv[i]))
 
