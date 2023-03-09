3c852087f0274ea9cabe443fa628c1fc9c82f98c
Noku <nnblb58@gmail.com>
2023-03-09 05:32:01 -0800

    changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
 
     counter = len(argv) - 1
-    if counter == 0:
+    if counter < 1:
         print("0 arguments.")
     elif counter == 1:
         print("1 argument:")
