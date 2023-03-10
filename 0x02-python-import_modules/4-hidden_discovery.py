ec9efb7c45f68f03beef3a1ad34521a9e45700af
Noku <nnblb58@gmail.com>
2023-03-10 01:22:17 -0800

    changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ != "__main__":
     import hidden_4
 
-    args = dir(hidden_4)
-    for name in args:
-        if name[:2] != "__":
-            print(name)
+    for index in dir(hidden_4):
+        if index in dir(hidden_4):
+            if index[:2] != '__':
+                print('{}'.format(index))
