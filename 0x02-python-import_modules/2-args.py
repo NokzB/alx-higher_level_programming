413085c160fcc4582cb8ee901e7b92ed0b73a44f
Noku <nnblb58@gmail.com>
2023-03-10 05:09:35 -0800

    removed elif statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(argv) < 1:
         print("0 arguments.")
     elif len(argv) == 1:
-        print("1 argument:".format(len(argv) - 1))
+        print("1 argument:")
     else:
         print("{:d} arguments:".format(len(argv) - 1))
 
