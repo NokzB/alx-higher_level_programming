7d03d312124f0159004242d69c2aadcf41a83ee6
Noku <nnblb58@gmail.com>
2023-03-10 04:35:41 -0800

    removed second print statement and changed elif to else

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
-'''#!/usr/bin/python3'''
+#!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
 
     if len(argv) < 1:
         print('0 arguments.')
     elif len(argv) == 1:
-        print('1 argument:')
-        print('1: {:s}'.format(len(argv[1]))
-    elif len(argv) => 2:
+        print('1 argument: {:s}'.format(len(argv) - 1))
+    else:
         print('{:d} arguments:'.format(len(argv) - 1))
 
     for arg in range(1, len(argv)):
