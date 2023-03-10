99b402ed1b5e423d074283330004f1a912aa859d
Noku <nnblb58@gmail.com>
2023-03-10 05:13:46 -0800

    changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) < 1:
+    import sys
+    arg = len(sys.argv)
+    if arg <= 1:
         print("0 arguments.")
-    elif len(argv) == 1:
-        print("1 argument:")
     else:
-        print("{:d} arguments:".format(len(argv) - 1))
-
-    for arg in range(1, len(argv)):
-        print("{:d}: {:s}.".format(arg, argv[arg]))
+        if arg == 2:
+            print("{:d} argument:".format(arg - 1))
+        else:
+            print("{:d} arguments:".format(arg - 1))
+        for i in range(1, arg):
+            print("{:d}: {}".format(i, sys.argv[i]))
