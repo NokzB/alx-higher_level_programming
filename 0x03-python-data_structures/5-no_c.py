467e7e44bf87592ad456535a7d88f135b30fbc8c
Noku <nnblb58@gmail.com>
2023-03-14 05:14:53 -0700

    seperated ord functions

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord("c"): None, ord("C"): None})
+    new_string = my_string.translate({ord('c'): None})
+    new_string2 = my_string.translate({ord('C'): None})
     print(new_string)
+    print(new_string2)
