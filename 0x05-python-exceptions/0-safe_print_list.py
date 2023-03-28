aaac767458d9c75a0d96c1c2114f0f1c846bceda
Noku <nnblb58@gmail.com>
2023-03-28 05:27:12 -0700

    added missing closing parantheses

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+
     try:
-        for i in range(x):
-            print ("{}".format(my_list[i], end="")
+        for y in range(x):
+            print ("{}".format(my_list[y]), end="")
     except IndexError:
         print()
-        return(i)
+        return y
     print()
     return x
