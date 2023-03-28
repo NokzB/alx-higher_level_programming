f8411667ba02241fc5c49681370ad40b8390bff3
Noku <nnblb58@gmail.com>
2023-03-28 05:54:15 -0700

    corrected indentation

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    for y in range(x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[y]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
             pass
         else:
             printed += 1
-        print()
-        return (printed)
+    print()
+    return (printed)
