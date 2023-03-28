6d6f586158b05dfa87206dd39742b1889a7770df
Noku <nnblb58@gmail.com>
2023-03-28 05:41:00 -0700

    corrections to except clause

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             j += 1
-        except (ValueError, TypeError):
+        except ValueError:
+            continue
+        except TypeError:
             continue
         print()
         return j
