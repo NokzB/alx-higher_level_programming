3f5c8ada70b171313e3a543c23071da613d2cb13
Noku <nnblb58@gmail.com>
2023-03-28 05:59:26 -0700

    corrected exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         quotient = a / b
-    except ArithmaticError:
+    except ArithmeticError:
         quotient = None
     finally:
         print("Inside result: {}".format(quotient))
