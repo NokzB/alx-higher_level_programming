31043e6c9eb2dbe72c652dcb1c065c4ae0436636
Noku <nnblb58@gmail.com>
2023-04-13 00:00:49 -0700

    initial commit for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """Defines a Square with size"""
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size
