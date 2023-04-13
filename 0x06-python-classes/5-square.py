eb65ffc877c504d7d57750397338989e35a2f9bc
Noku <nnblb58@gmail.com>
2023-04-13 03:37:27 -0700

    initial commit for task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Square class with size"""
+    def __init__(self, size=0):
+        """Initializes the data"""
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the Square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieves the size of a square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of a square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the Square"""
+        if self.__size == 0:
+            print()
+        else:
+            sqr = "#" * self.__size
+            for i in range(self.__size):
+                print(sqr)
