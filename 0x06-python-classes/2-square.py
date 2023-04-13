459e6e3647995ef199c907b8d9120bb889794482
Noku <nnblb58@gmail.com>
2023-04-13 00:22:59 -0700

    corrected second if statement

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,11 @@
 
 
 class Square:
+    """Square class with size"""
     def __init__(self, size=0):
         """Initializes the data"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if type(size) < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
