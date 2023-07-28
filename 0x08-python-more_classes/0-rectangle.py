c04443aa488d274aaa1c44d937127a9aa4f423df
Nokuphila Bhembe <nnblb58@gmail.com>
2023-07-28 09:33:39 +0200

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""A function that defines a rectangle class"""
-
-
-class Rectangle:
-    """Defines a Rectangle"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-More projects using classes and objects
