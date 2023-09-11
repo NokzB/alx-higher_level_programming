1e28c4f01b671951b5aeeeba13c1bf678410480b
Nokuphila <nnblb58@gmail.com>
2023-09-11 06:06:13 -0700

    initial commit for task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class that will inherit from the list"""
+
+
+class MyList():
+    """Prints a sorted list in ascending order for the built-in list class"""
+    def print_sorted(self):
+        """prints a list sorted in ascending order"""
+        print(sorted())
