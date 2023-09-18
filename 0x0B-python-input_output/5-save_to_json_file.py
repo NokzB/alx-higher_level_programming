450f60e229b0fd5e76a662980182497023aaf65f
Nokuphila <nnblb58@gmail.com>
2023-09-18 05:25:11 -0700

    changes

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, flename):
     """ Function to save an object to a text file using json."""
     with open(filename, "w") as f:
-        json.dump(my_obj, f)
+        json.dumps(my_obj, f)
