447f70720431c1ab1158b53a697d3b545c4c9a1b
Nokuphila <nnblb58@gmail.com>
2023-09-18 05:11:15 -0700

    initial commit for task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function that returns the JSON representation of an obejct"""
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object
+
+    Arguments:
+        My_obj: The object to serialize
+    """
+    return json.dumps(my_obj)
