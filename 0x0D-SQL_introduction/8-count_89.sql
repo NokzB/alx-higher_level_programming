cd66655785ac390f1c9714d577a2a3246e4a6120
Nokuphila <nnblb58@gmail.com>
2023-10-19 09:44:14 -0700

    attempting to fix errors

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,2 @@
 -- displays the number of records with id = 89 from table first_table in MySQL
-
-SELECT COUNT (id)
-FROM first_table
-WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
