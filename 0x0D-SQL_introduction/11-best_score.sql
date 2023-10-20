12692aa84b4323077e4b5e7b224d83ffbfaf56c1
Nokuphila <nnblb58@gmail.com>
2023-10-20 00:38:07 -0700

    initial commit for task 11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table
+-- display's score and name
+-- displays scores greater or equal to 10
+-- displays in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
