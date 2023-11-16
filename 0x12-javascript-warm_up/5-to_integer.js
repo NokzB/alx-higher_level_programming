8ad948128a2a920e1a51310cbfc1606003e72f22
Nokuphila <nnblb58@gmail.com>
2023-11-16 01:16:48 -0800

    checking if is not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 // Script to print <My Number: "number"> if first arg can be converted to int.
 
-if (!isNaN(process.argv[2])) {
+if (!isNaN(parseInt(process.argv[2]))) {
   console.log('My number:', parseInt(process.argv[2]));
 } else {
   console.log('Not a Number');
