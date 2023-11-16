8404c162b2612199eaede4b675efa4e1a6ea1056
Nokuphila <nnblb58@gmail.com>
2023-11-16 01:45:00 -0800

    initial commir for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Script that prints multiple strings using a loop.
+
+let i;
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
