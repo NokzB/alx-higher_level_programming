1061502963583d14b9a6575947f3f73f4646a2b7
Nokuphila <nnblb58@gmail.com>
2023-11-17 00:05:49 -0800

    changed for loops into a while loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i += 1) {
-    let line = '';
-    for (let j = 0; i < size; j += 1) line += 'X';
-    console.log(line);
+  let i = 0;
+  while (i < size) {
+    console.log('X'.repeat(size));
+    i++;
   }
 }
