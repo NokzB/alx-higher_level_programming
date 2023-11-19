af454d1f10edd2f2646eb22cb36bef3ccb590847
Nokuphila <nnblb58@gmail.com>
2023-11-19 10:59:49 -0800

    initial commit for task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to list all State objects from the database"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    """Accesses the database to get the states"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(State).order_by(State.id):
+            print('{0}: {1}'.format(instance.id, instance.name)) 
