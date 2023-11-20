e8078ebe963838b6b3401d07702c4f5050b58d3c
Nokuphila <nnblb58@gmail.com>
2023-11-20 05:19:45 -0800

    initial commit for task 12, no errors

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script to update State objects from the database"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    """Accesses the database to get the states"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter(State.id== '2').first()
+    update_state.name = 'New Mexico'
+    session.commit()
+    session.close()
